473bd7453b4a8e4d1411738cd1118f2e9b47b332
anafevictorio2k26 <anafevictorio2k26@gmail.com>
2026-06-15 15:29:46 +0800

    create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,4 +1,4 @@
-async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -12,35 +12,14 @@ async function handler(req, res) {
   }
 
   try {
-    const { amount } = req.body;
-
-    const response = await fetch(
-      "https://api.paymongo.com/v1/checkout_sessions",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization:
-            "Basic " +
-            Buffer.from(process.env.PAYMONGO_SECRET_KEY + ":").toString("base64"),
-        },
-        body: JSON.stringify({
-          data: {
-            attributes: {
-              amount,
-              currency: "PHP",
-              payment_method_types: ["gcash", "card"],
-              success_url: "https://your-firebase-site.web.app/download.html",
-              cancel_url: "https://your-firebase-site.web.app/cancel.html",
-            },
-          },
-        }),
-      }
-    );
-
-   const data = await response.json();
-
-return res.status(response.status).json(data);
+    return res.status(200).json({
+      success: true,
+      body: req.body,
+      keyExists: !!process.env.PAYMONGO_SECRET_KEY
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: err.message
     });
   }
 }
