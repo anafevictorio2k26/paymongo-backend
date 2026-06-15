fdfb70d853b84ef712fbe1d010e4c00a422da939
anafevictorio2k26 <anafevictorio2k26@gmail.com>
2026-06-15 15:21:28 +0800

    create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -38,12 +38,9 @@ export default async function handler(req, res) {
       }
     );
 
-    const data = await response.json();
+   const data = await response.json();
 
-    return res.status(200).json(data);
-  } catch (err) {
-    return res.status(500).json({
-      error: err.message,
+return res.status(response.status).json(data);
     });
   }
 }
